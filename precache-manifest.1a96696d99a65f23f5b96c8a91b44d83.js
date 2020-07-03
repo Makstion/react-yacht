1371e116cfbc9020a03a2549b12034c127d6619f
Maksim Vdovin <vdvmaxim@gmail.com>
2020-07-04 01:06:03 +0500

    Updates

---
+++ precache-manifest.1a96696d99a65f23f5b96c8a91b44d83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad81852c441912b28d3c7c7fd1c163b4",
+    "url": "/react-yacht/index.html"
+  },
+  {
+    "revision": "a512ea55cca51837c481",
+    "url": "/react-yacht/static/css/main.8d98a148.chunk.css"
+  },
+  {
+    "revision": "974824f1f2e06805a8e0",
+    "url": "/react-yacht/static/js/2.896dcc39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-yacht/static/js/2.896dcc39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a512ea55cca51837c481",
+    "url": "/react-yacht/static/js/main.769460b5.chunk.js"
+  },
+  {
+    "revision": "fe07b2daa6bc9b0e2aa3",
+    "url": "/react-yacht/static/js/runtime-main.454c476f.js"
+  }
+]);
