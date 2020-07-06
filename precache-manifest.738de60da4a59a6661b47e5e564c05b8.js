8bac20eac4791396d8cae41e999b24c54d2b7c2c
Maksim Vdovin <vdvmaxim@gmail.com>
2020-07-06 21:06:15 +0500

    Updates

--- precache-manifest.738de60da4a59a6661b47e5e564c05b8.js
+++ precache-manifest.738de60da4a59a6661b47e5e564c05b8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6bf2c68146d441a26427e4690030c354",
+    "revision": "165980560bb46e2a7660ff1dd9d8f38c",
     "url": "/react-yacht/index.html"
   },
   {
-    "revision": "9703711841c58ac53454",
+    "revision": "2b139d260ae02c14d99d",
     "url": "/react-yacht/static/css/main.c5b5c617.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-yacht/static/js/2.f1a72e6b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9703711841c58ac53454",
-    "url": "/react-yacht/static/js/main.c49e99ee.chunk.js"
+    "revision": "2b139d260ae02c14d99d",
+    "url": "/react-yacht/static/js/main.12a246fb.chunk.js"
   },
   {
     "revision": "fe07b2daa6bc9b0e2aa3",
