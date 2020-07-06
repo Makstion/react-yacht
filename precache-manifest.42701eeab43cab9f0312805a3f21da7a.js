998ff44619ebdf7825846a1cea320a96c3bd9027
Maksim Vdovin <vdvmaxim@gmail.com>
2020-07-06 20:58:10 +0500

    Updates

--- precache-manifest.42701eeab43cab9f0312805a3f21da7a.js
+++ precache-manifest.42701eeab43cab9f0312805a3f21da7a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4346e41df99e9201a3db2f52a4e5bacc",
+    "revision": "10eebfdca37da337db944d74765955dd",
     "url": "/react-yacht/index.html"
   },
   {
-    "revision": "feb680837411b9e5877d",
+    "revision": "b4d8c5ba55a8769aa5e2",
     "url": "/react-yacht/static/css/main.c5b5c617.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-yacht/static/js/2.f1a72e6b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "feb680837411b9e5877d",
-    "url": "/react-yacht/static/js/main.de456195.chunk.js"
+    "revision": "b4d8c5ba55a8769aa5e2",
+    "url": "/react-yacht/static/js/main.d07ebfa0.chunk.js"
   },
   {
     "revision": "fe07b2daa6bc9b0e2aa3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-yacht/precache-manifest.710711db1d928c973acb8f0d9c00faf7.js"
+  "/react-yacht/precache-manifest.42701eeab43cab9f0312805a3f21da7a.js"
 );
 
 self.addEventListener('message', (event) => {
