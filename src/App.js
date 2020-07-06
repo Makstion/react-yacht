dba1b4cf92227621f9a67a73fd3649e516e16b54
Maksim Vdovin <vdvmaxim@gmail.com>
2020-07-06 21:04:28 +0500

    base name

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
 
-        <BrowserRouter>
+        <BrowserRouter basename="/react-yacht/.">
             <div className="app-wrapper">
                 <HeaderContainer />
                 <Navbar />
